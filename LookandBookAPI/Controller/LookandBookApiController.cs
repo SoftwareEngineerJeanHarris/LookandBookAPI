9be3d1779485ba45ccd0e3da587ddfef84c5b273
Jean <JeanMichael.Harris@gmail.com>
2025-03-30 18:24:59 -0400

    Controller setup.

---
+++ LookandBookAPI/Controller/LookandBookApiController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using LookandBookAPI.Models;
+using LookandBookAPI.Data;
+
+namespace LookandBookAPI.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LookandBookApiController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public LookandBookApiController(ApiContext context)
+        {
+            _context = context;
+        }
+    }
+}
