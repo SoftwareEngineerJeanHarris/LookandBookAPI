71057b9a9a0b490d8429ca163689ad9427197dca
Jean <JeanMichael.Harris@gmail.com>
2025-04-05 13:16:04 -0400

    CreateEdit Get

--- LookandBookAPI/Controller/LookandBookApiController.cs
+++ LookandBookAPI/Controller/LookandBookApiController.cs
@@ -15,5 +15,41 @@ namespace LookandBookAPI.Controller
         {
             _context = context;
         }
+
+        // Create/Edit
+        [HttpPost]
+        public JsonResult CreateEdit(HotelBooking booking)
+        {
+            if(booking.Id == 0)
+            {
+                _context.HotelBooking.Add(booking);
+            }
+            else
+            {
+                var bookingInDb = _context.HotelBooking.Find(booking.Id);
+
+                if (bookingInDb == null)
+                    return new JsonResult(NotFound());
+
+                bookingInDb = booking;
+            }
+
+            _context.SaveChanges();
+
+            return new JsonResult(Ok(booking));
+        }
+
+        // Get
+        [HttpGet]
+        public JsonResult Get(int id)
+        {
+            var result = _context.HotelBooking.Find(id);
+
+            if (result == null)
+                return new JsonResult(NotFound());
+
+            return new JsonResult(Ok(result));
+
+        }
     }
 }
